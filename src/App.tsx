db09e8b992eaceb8339c91597558a80c958a4d34
Andrey Kupetskov <amater131089@gmail.com>
2026-07-10 17:05:22 +0300

    Add basic slyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
 export function App() {
-    return <p className="text-amber-500">Hello</p>;
+    return (
+        <div className="bg-surface-primary m-6 p-2 border border-border rounded-large">
+            <h1 className="text-text-primary">Header</h1>
+            <p className="text-text-secondary">Secondary text</p>
+            <p className="text-text-info">Info text</p>
+            <input
+                type="text"
+                className="p-1 text-input-text bg-input-bg border border-input-border placeholder:text-input-placeholder"
+                placeholder="placeholder"
+            />
+            <br />
+            <br />
+            <input
+                type="text"
+                className="p-1 text-input-text bg-input-bg border border-input-border placeholder:text-input-placeholder"
+                defaultValue="default"
+            />
+            <br />
+            <br />
+            <button className="p-1 text-button-primary-text bg-button-primary-bg border border-button-primary-border hover:text-button-primary-text-hover hover:bg-button-primary-bg-hover rounded-medium">
+                Primary button
+            </button>
+        </div>
+    );
 }
