32bc99b16020db718b6cc8d4f1ecc9c501118516
Andrey Kupetskov <amater131089@gmail.com>
2026-07-13 16:55:24 +0300

    add Header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,12 @@
-import { Outlet, NavLink } from "react-router";
+import { Outlet } from "react-router";
 
-import { ROUTE_PATHS } from "@/app/router/paths.type";
+import { Header } from "@/shared/components/Header";
 
 export function MainLayout() {
     return (
         <div className="h-svh flex flex-col">
-            <header className="p-2">
-                <NavLink to={ROUTE_PATHS.HOME}>Home</NavLink>
-                <br />
-                <NavLink to={ROUTE_PATHS.PROJECT}>Project</NavLink>
-                <br />
-                <NavLink to={`${ROUTE_PATHS.PROJECT}/13`}>Project 13</NavLink>
-            </header>
+            <Header />
+
             <main className="min-h-0 grow">
                 <Outlet />
             </main>
---
+++ src/shared/components/Header.tsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router";
+
+import { Logo } from "./Logo";
+
+import { ROUTE_PATHS } from "@/app/router/paths.type";
+
+export function Header() {
+    return (
+        <header className="flex items-center gap-6 p-2">
+            <Logo />
+
+            <NavLink to={ROUTE_PATHS.HOME}>Home</NavLink>
+            <NavLink to={ROUTE_PATHS.PROJECT}>Project</NavLink>
+            <NavLink to={`${ROUTE_PATHS.PROJECT}/13`}>Project 13</NavLink>
+        </header>
+    );
+}
