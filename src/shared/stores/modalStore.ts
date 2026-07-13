6734ac1f6e6d10c32ca693c8a7e5185c5807d7d3
Andrey Kupetskov <amater131089@gmail.com>
2026-07-13 19:05:57 +0300

    change to use immer

--- src/shared/stores/modalStore.ts
+++ src/shared/stores/modalStore.ts
@@ -1,9 +1,5 @@
 import { create } from "zustand";
-// import { persist, createJSONStorage } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-
-// import { sharedStorage } from "@/shared/lib/sharedStorage";
-// import { STORAGE_KEY } from "../config/env";
 
 interface ModalState {
     isOpen: boolean;
@@ -13,15 +9,18 @@ interface ModalState {
 }
 
 export const useModalStore = create<ModalState>()(
-    // persist(
     immer((set) => ({
         isOpen: false,
         content: null,
-        openModal: (content) => set({ isOpen: true, content }),
-        closeModal: () => set({ isOpen: false, content: null }),
+        openModal: (content) =>
+            set((state) => {
+                state.isOpen = true;
+                state.content = content;
+            }),
+        closeModal: () =>
+            set((state) => {
+                state.isOpen = false;
+                state.content = null;
+            }),
     })),
-    //     { name: STORAGE_KEY, storage: createJSONStorage(() => sharedStorage) },
-    // ),
 );
-
-// TODO: remove persist
