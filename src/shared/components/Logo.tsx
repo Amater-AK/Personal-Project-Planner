21749fa8833c3c42d6513af08f2bae56bfbcf3f2
Andrey Kupetskov <amater131089@gmail.com>
2026-07-17 15:41:29 +0300

    fix(Logo): add letter spacing

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
     return (
         <div className="flex items-center gap-2">
-            <span className="font-semibold uppercase">Project Planner</span>
+            <span className="font-semibold tracking-[0.35em] uppercase">Project Planner</span>
         </div>
     );
 }
