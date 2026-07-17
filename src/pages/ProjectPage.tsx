e8d08f22ad228d71943cd166b4b2e70dc250d327
Andrey Kupetskov <amater131089@gmail.com>
2026-07-17 16:08:01 +0300

    Set basic ProjectPage

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,39 +1,33 @@
 import { useParams } from "react-router";
 
-import { ActionMenu } from "@/shared/components/ui/ActionMenu";
-import { Button } from "@/shared/components/ui/Button";
-import { buttonStyles } from "@/shared/styles/buttonStyles";
+import { BsArrowLeftShort } from "react-icons/bs";
+
+import { useProjectStore } from "@/features/projects/stores/projectStore";
+
+import { LinkButton } from "@/shared/components/ui/LinkButton";
+
+import { ROUTE_PATHS } from "@/app/router/paths";
 
 export function ProjectPage() {
     const { pId } = useParams();
+    const getProject = useProjectStore((state) => state.getProject);
+
+    const project = getProject(pId);
 
     return (
-        <div>
-            <h1>Project id: {pId}</h1>
-            <br />
-            <ActionMenu
-                trigger={
-                    <Button intent="secondary" onlyIcon={true}>
-                        ...
-                    </Button>
-                }
-                className={(isOpen) =>
-                    `top-0 left-full min-w-40 p-2 bg-stone-300 transition-all duration-300 z-10 ${isOpen ? "translate-y-0" : "translate-y-10 opacity-0"}`
-                }
-            >
-                <ActionMenu.ActionButton
-                    className={buttonStyles({ intent: "regular" })}
-                    onClick={() => console.log("Action 1")}
-                >
-                    Action 1
-                </ActionMenu.ActionButton>
-                <ActionMenu.ActionButton className={buttonStyles({ intent: "regular" })}>
-                    Action 2
-                </ActionMenu.ActionButton>
-                <ActionMenu.ActionButton className={buttonStyles({ intent: "regular" })}>
-                    Action 3
-                </ActionMenu.ActionButton>
-            </ActionMenu>
+        <div className="flex flex-col gap-4 h-full px-2">
+            <header className="grid grid-cols-3 items-center gap-4">
+                <div>
+                    <LinkButton to={ROUTE_PATHS.HOME} intent="secondary">
+                        <BsArrowLeftShort />
+                        <span>Back</span>
+                    </LinkButton>
+                </div>
+
+                <h1 className="truncate w-full text-center text-text-info">{project.title}</h1>
+            </header>
+
+            <section className="grow min-h-0">Columns</section>
         </div>
     );
 }
