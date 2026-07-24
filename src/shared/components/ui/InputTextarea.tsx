e54583066bbfec636e95faf390024ad9d12aa2e7
Andrey Kupetskov <amater131089@gmail.com>
2026-07-24 16:38:44 +0300

    fix(InputTextarea): add resize none

--- src/shared/components/ui/InputTextarea.tsx
+++ src/shared/components/ui/InputTextarea.tsx
@@ -3,7 +3,7 @@ interface Props extends React.ComponentProps<"textarea"> {}
 export function InputTextarea({ ...props }: Props) {
     return (
         <textarea
-            className="w-full px-2 py-1 text-input-text bg-input-bg border border-input-border rounded-medium placeholder:text-input-placeholder"
+            className="w-full px-2 py-1 text-input-text bg-input-bg border border-input-border rounded-medium resize-none placeholder:text-input-placeholder"
             {...props}
         ></textarea>
     );
